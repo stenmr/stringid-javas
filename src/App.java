63199a91c24c0cd4421fb88f8f46b6aa9b62a351
stenmr <stenmr@tlu.ee>
2020-05-12 09:22:49 +0300

    henry's string comparison demonstration & stringbuilder methods

--- src/App.java
+++ src/App.java
@@ -10,7 +10,26 @@ public class App {
 
         System.out.println("String.concat(\"\"): " + simpleConcat(1_000_000_000, "") + "ms");
 
-        System.out.println("String.concat(\"s\"): " + simpleConcat(100_000, "s") + "ms");
+        System.out.println("String.concat(\"s\"): " + simpleConcat(500_000, "s") + "ms");
+
+        // StringBuffer is mutable by any type of changes in the existing object
+        StringBuffer sbf = new StringBuffer("Henry");
+        sbf.append(" is a nice guy");
+        sbf.replace(6, 8, "is not");
+        System.out.println(sbf);
+        sbf.delete(5, 12);
+        sbf.replace(6, 6, "is ");
+        sbf.reverse();
+        System.out.println(sbf);
+
+        // why to use equal() instead of == when you want to compare Strings
+        // == address comparison
+        // equals() content comparison
+
+        String s1 = new String("HELLO");
+        String s2 = new String("HELLO");
+        System.out.println("HELLO == HELLO: " + s1 == s2);
+        System.out.println("s1.equals(s2): " + s1.equals(s2));
     }
 
     public static long simpleConcat(int amount, String append) {
