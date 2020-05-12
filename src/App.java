dc346ca713bae20d9255aa3c6c51fa79f89906b6
stenmr <stenmr@tlu.ee>
2020-05-12 09:46:31 +0300

    small adjustments

--- src/App.java
+++ src/App.java
@@ -5,13 +5,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("StringBuilder: " + stringBuilderConcat(1_000_000_000, "") + "ms");
-
         System.out.println("StringBuffer: " + stringBufferConcat(1_000_000_000, "") + "ms");
-
         System.out.println("String.concat(\"\"): " + simpleConcat(1_000_000_000, "") + "ms");
-
         System.out.println("String.concat(\"s\"): " + simpleConcat(500_000, "s") + "ms");
-
+        
         // StringBuffer is mutable by any type of changes in the existing object
         StringBuffer sbf = new StringBuffer("Henry");
         sbf.append(" is a nice guy");
@@ -21,11 +18,10 @@ public class App {
         sbf.replace(6, 6, "is ");
         sbf.reverse();
         System.out.println(sbf);
-
+        
         // why to use equal() instead of == when you want to compare Strings
         // == address comparison
         // equals() content comparison
-
         String s1 = new String("HELLO");
         String s2 = new String("HELLO");
         System.out.println("HELLO == HELLO: " + s1 == s2);
@@ -40,9 +36,7 @@ public class App {
         for (int i = 0; i < amount; i++) {
             test = test.concat(append);
         }
-
         return beforeTime.until(LocalTime.now(), ChronoUnit.MILLIS);
-
     }
 
     public static long stringBuilderConcat(int amount, String append) {
@@ -53,7 +47,6 @@ public class App {
         for (int i = 0; i < amount; i++) {
             test.append(append);
         }
-
         return beforeTime.until(LocalTime.now(), ChronoUnit.MILLIS);
     }
 
